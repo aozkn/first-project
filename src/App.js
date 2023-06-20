f51fd65baae8c3e4c6eb15d6005e9cd962601e75
Ali Ozkan <aliozkan4334@gmail.com>
2023-06-20 16:24:57 +0200

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Header from "./header";
+
 
 
 function App() {
+//*Js alani
+// !birder fazla element yazacagimiz zaman yazilacak elementleri bir kapsiyaciya eklemek gerekiyor yoksa return elementleri almaz.
   return (
-    <div >
-  
-    </div>
+    <>
+    
+   <Header/>
+    
+    </>
+    //* JSX formatinda yazmak gerekiyor bu alanda
+    
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,5 @@
+function Header(){
+    return <div><h1> hello react</h1></div> 
+}
+
+export default Header;
